f8a76a1baaf433602cf0be22e5ece04d15615fa0
Charlie Astrada <carlosastrada@gmail.com>
2020-11-19 23:31:00 -0300

    Add mikroorm config to allow using mikroorm CLI
    Mikro-orm CLI used for running migrations. Note that the following typing strategy was used on the mikro-orm config: Parameters<typeof MikroORM.init>[0]. This allows us to type the config to the Parameters of the MikroORM.init function. Since parameters returns an array, we get the first item in the array.

---
+++ dist/mikro-orm.config.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const Post_1 = require("./entities/Post");
+const path_1 = __importDefault(require("path"));
+exports.default = {
+    clientUrl: "postgresql://charlieastrada@localhost:5432/spensa",
+    debug: process.env.NODE_ENV !== "production",
+    entities: [Post_1.Post],
+    migrations: {
+        path: path_1.default.join(__dirname, "./migrations"),
+        pattern: /^[\w-]+\d+\.[tj]s$/,
+    },
+    type: "postgresql",
+};
+//# sourceMappingURL=mikro-orm.config.js.map
