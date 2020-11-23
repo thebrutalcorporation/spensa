c9cf5aaa5a042e408fb16cd1f84161e4f934d710
Charlie Astrada <carlosastrada@gmail.com>
2020-11-22 23:27:23 -0300

    Configure apollo server and type-graphql
    ApolloServer is a server that runs on top of express in our case and handles the graphql server running at the /graphql endpoint. type-graphql is used for generating the graphql schema based on the resolvers.
    
    - A very simple "hello world" resolver created to get a starting point server up and running.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@ import { MikroORM } from "@mikro-orm/core";
 import mikroConfig from "./mikro-orm.config";
 import express from "express";
 import "dotenv-safe/config";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -9,6 +12,15 @@ const main = async () => {
 
   const port = parseInt(process.env.PORT);
   const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app }); //create graphql endpoint on express
 
   app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
