60216dfe0be09f9888766433a2154d69c68a6d6f
Charlie Astrada <carlosastrada@gmail.com>
2020-11-26 17:28:51 -0300

    Remove Post entity and old migrations
    Post entity is not an entity that will be used. It was simply a placeholder. The old migrations are also unnecessary since Post is not to be used in the project. Keeping the old migrations runs the risk of being confusing for other devs, so removing.

--- src/entities/Post.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, Property } from "@mikro-orm/core";
-import { BaseEntity } from "./BaseEntity";
-
-@Entity()
-export class Post extends BaseEntity {
-  @Property({ type: "text" })
-  title!: string;
-}
