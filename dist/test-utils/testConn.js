1d2ac50600b77d36dd635550cc4601b8b29e55c8
Charlie Astrada <carlosastrada@gmail.com>
2020-12-25 19:19:04 -0300

    Remove migration params from mikroOrm.init call.
    migration params are not needed with test connection since the test setup.ts uses getSchemaGenerator function to programatically drop and create the schema, which is done to clean ouot the db and re-generate schema before the test run.

--- dist/test-utils/testConn.js
+++ dist/test-utils/testConn.js
@@ -1,12 +1,8 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.testConn = void 0;
 const core_1 = require("@mikro-orm/core");
 const child_process_1 = require("child_process");
-const path_1 = __importDefault(require("path"));
 const BaseEntity_1 = require("../entities/BaseEntity");
 const Transaction_1 = require("../entities/Transaction");
 exports.testConn = () => {
@@ -14,10 +10,6 @@ exports.testConn = () => {
     return core_1.MikroORM.init({
         clientUrl: "postgresql://charlieastrada@localhost:5432/spensa-test",
         entities: [BaseEntity_1.BaseEntity, Transaction_1.Transaction],
-        migrations: {
-            path: path_1.default.join(__dirname, "../migrations"),
-            pattern: /^[\w-]+\d+\.[tj]s$/,
-        },
         type: "postgresql",
     });
 };
--- src/test-utils/testConn.ts
+++ src/test-utils/testConn.ts
@@ -1,6 +1,5 @@
 import { MikroORM } from "@mikro-orm/core";
 import { exec } from "child_process";
-import path from "path";
 import { BaseEntity } from "../entities/BaseEntity";
 import { Transaction } from "../entities/Transaction";
 
@@ -11,10 +10,6 @@ export const testConn = () => {
     clientUrl: "postgresql://charlieastrada@localhost:5432/spensa-test", //TODO: use env variables
     // debug: process.env.NODE_ENV !== "production",
     entities: [BaseEntity, Transaction],
-    migrations: {
-      path: path.join(__dirname, "../migrations"),
-      pattern: /^[\w-]+\d+\.[tj]s$/,
-    },
     type: "postgresql",
   });
 };
