2c7b2bcd7d0feb0672ad42a3d75442e71f4065a5
Charlie Astrada <carlosastrada@gmail.com>
2020-12-28 00:04:11 -0300

    Add User entity to mikro-orm config and test-config
    The User entity is required for the schema to be created on server start and test connection setup.

--- dist/mikro-orm.config.js
+++ dist/mikro-orm.config.js
@@ -6,10 +6,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = __importDefault(require("path"));
 const BaseEntity_1 = require("./entities/BaseEntity");
 const Transaction_1 = require("./entities/Transaction");
+const User_1 = require("./entities/User");
 exports.default = {
     clientUrl: "postgresql://charlieastrada@localhost:5432/spensa",
     debug: process.env.NODE_ENV !== "production",
-    entities: [Transaction_1.Transaction, BaseEntity_1.BaseEntity],
+    entities: [BaseEntity_1.BaseEntity, Transaction_1.Transaction, User_1.User],
     migrations: {
         path: path_1.default.join(__dirname, "./migrations"),
         pattern: /^[\w-]+\d+\.[tj]s$/,
--- dist/test-utils/testConn.js
+++ dist/test-utils/testConn.js
@@ -5,11 +5,12 @@ const core_1 = require("@mikro-orm/core");
 const child_process_1 = require("child_process");
 const BaseEntity_1 = require("../entities/BaseEntity");
 const Transaction_1 = require("../entities/Transaction");
+const User_1 = require("../entities/User");
 exports.testConn = () => {
     child_process_1.exec(`createdb spensa-test`);
     return core_1.MikroORM.init({
         clientUrl: "postgresql://charlieastrada@localhost:5432/spensa-test",
-        entities: [BaseEntity_1.BaseEntity, Transaction_1.Transaction],
+        entities: [BaseEntity_1.BaseEntity, Transaction_1.Transaction, User_1.User],
         type: "postgresql",
     });
 };
