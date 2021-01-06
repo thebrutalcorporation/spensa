56dd4192504547db0c10d59f6ba1a1017d5b3542
Charlie Astrada <carlosastrada@gmail.com>
2021-01-06 12:54:03 -0300

    Add login mutation
    Login mutation logs user in after checking that username exists in the database and the password is valid.

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,5 +1,13 @@
 import argon2 from "argon2";
-import { Arg, Ctx, Field, InputType, Mutation, Resolver } from "type-graphql";
+import {
+  Arg,
+  Ctx,
+  Field,
+  InputType,
+  Mutation,
+  ObjectType,
+  Resolver,
+} from "type-graphql";
 import { Context } from "vm";
 import { User } from "../entities/User";
 
@@ -12,19 +20,76 @@ class UsernamePasswordInput {
   password: string;
 }
 
+@ObjectType()
+class FieldError {
+  @Field()
+  field: string;
+
+  @Field()
+  message: string;
+}
+
+@ObjectType()
+class UserResponse {
+  @Field(() => [FieldError], { nullable: true })
+  errors?: FieldError[];
+
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
+  @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
     @Ctx() { em }: Context
   ) {
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
-      username: options.username,
+      username: options.username.toLowerCase(),
       password: hashedPassword,
     });
     await em.persistAndFlush(user);
     return user;
+  }
+
+  @Mutation(() => UserResponse)
+  async login(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: Context
+  ): Promise<UserResponse> {
+    const user: User = await em.findOne(User, {
+      username: options.username.toLowerCase(),
+    });
+
+    if (!user) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "username does not exist!",
+          },
+        ],
+      };
+    }
+
+    const isValidPassword = await argon2.verify(
+      user.password,
+      options.password
+    );
+
+    if (!isValidPassword) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Invalid login!",
+          },
+        ],
+      };
+    }
+
+    return { user };
   }
 }
