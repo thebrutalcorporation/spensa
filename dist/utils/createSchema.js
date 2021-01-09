4830d0015332d3a89adee4991188afa68e2e235e
Charlie Astrada <carlosastrada@gmail.com>
2021-01-09 20:43:36 -0300

    Add user resolver to createSchema

--- dist/utils/createSchema.js
+++ dist/utils/createSchema.js
@@ -1,13 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createSchema = void 0;
-const hello_1 = require("../resolvers/hello");
 const transaction_1 = require("../resolvers/transaction");
 const type_graphql_1 = require("type-graphql");
 const user_1 = require("../resolvers/user");
 exports.createSchema = () => {
     return type_graphql_1.buildSchema({
-        resolvers: [hello_1.HelloResolver, transaction_1.TransactionResolver, user_1.UserResolver],
+        resolvers: [transaction_1.TransactionResolver, user_1.UserResolver],
         validate: false,
     });
 };
--- src/utils/createSchema.ts
+++ src/utils/createSchema.ts
@@ -1,11 +1,10 @@
-import { HelloResolver } from "../resolvers/hello";
 import { TransactionResolver } from "../resolvers/transaction";
 import { buildSchema } from "type-graphql";
 import { UserResolver } from "../resolvers/user";
 
 export const createSchema = () => {
   return buildSchema({
-    resolvers: [HelloResolver, TransactionResolver, UserResolver],
+    resolvers: [TransactionResolver, UserResolver],
     validate: false,
   });
 };
