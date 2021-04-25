fcba9db2aea5229cefa2129d1e03126b366c6790
Charlie Astrada <carlosastrada@gmail.com>
2021-04-24 23:10:48 -0300

    Add login functionality
    * Add /login page
    * Add NavBar which shows username if logged in, otherwise shows Login and Register links

---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,36 @@
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import React from "react";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+
+interface NavBarProps {}
+
+export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+
+  if (fetching) {
+    return null;
+  }
+
+  return (
+    <Flex bg="tomato" p={4}>
+      <Box ml={"auto"}>
+        {!data?.me ? (
+          <>
+            <NextLink href="/login">
+              <Link mr={2}>Login</Link>
+            </NextLink>
+            <NextLink href="/register">
+              <Link>Register</Link>
+            </NextLink>
+          </>
+        ) : (
+          <Flex>
+            <Box mr={2}>{data.me.username}</Box>
+            <Button variant="link">Logout</Button>
+          </Flex>
+        )}
+      </Box>
+    </Flex>
+  );
+};
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,3 +1,14 @@
-const Index = () => <div>Hello World!</div>;
+import { Box } from "@chakra-ui/layout";
+import React from "react";
+import { NavBar } from "../components/NavBar";
+
+const Index = () => (
+  <Box>
+    <NavBar />
+    <Box>
+      <div>Hello World!</div>
+    </Box>
+  </Box>
+);
 
 export default Index;
