959a62ec52b9f7d1a5b552a808858d7ecf2551bb
Charlie Astrada <carlosastrada@gmail.com>
2021-04-28 00:20:29 -0300

    Add SSR to login page.
    After adding URQL SSR package, for queries to work, we need to add SSR (without enabling SSR) higher order component for the query to work. If we wanted SSR to be enabled, we would pass in the {ssr: true} object to the higher order component as the second argument. In this case, the higher order component is used so that the query works but we do not need to actually enable SSR.

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -1,11 +1,12 @@
 import { Box, Button } from "@chakra-ui/react";
 import { Form, Formik } from "formik";
+import { withUrqlClient } from "next-urql";
 import { useRouter } from "next/router";
 import React from "react";
 import InputField from "../components/InputField";
-import { NavBar } from "../components/NavBar";
 import Wrapper from "../components/Wrapper";
 import { useLoginMutation } from "../generated/graphql";
+import { createUrqlClient } from "../utils/createUrqlClient";
 import { toErrorMap } from "../utils/toErrorMap";
 
 interface loginProps {}
@@ -57,4 +58,4 @@ const Login: React.FC<loginProps> = ({}) => {
   );
 };
 
-export default Login;
+export default withUrqlClient(createUrqlClient)(Login);
