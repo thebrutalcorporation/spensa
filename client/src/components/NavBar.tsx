d7e778d2d3298b3a934ce1a0be04720ae736db73
Charlie Astrada <carlosastrada@gmail.com>
2021-04-29 23:29:40 -0300

    Fix issue with redundant query for SSR pages
    When pages are set to SSR, any query on a component that runs on the page will run on the nextjs server due to how SSR works. In this case NavBar runs the me query on the server, but since the server is not storing a cookie, the Me query will always return null. This can be fixed by passing the cookie to the server, but it's better to just avoid running teh me query on the server altogether by using the pause attribute.
    
    * create an isServer util since we want to pause the query only when we're on the server but let it run if were on the client. The isServer function checks for the window object, which would only exist on the client. If typeof is undefined, then we're on the server.

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -2,12 +2,17 @@ import { Box, Button, Flex, Link } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
 import { useLogoutMutation, useMeQuery } from "../generated/graphql";
+import { isServer } from "../utils/isServer";
 
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
   const [{ fetching: isFetchingLogout }, logout] = useLogoutMutation();
-  const [{ data, fetching }] = useMeQuery();
+  const [{ data, fetching }] = useMeQuery({
+    pause: isServer(), //do not run query if on server
+  });
+
+  console.log("data: ", data);
 
   if (fetching) {
     return null;
