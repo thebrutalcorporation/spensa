c333645b9829c4711457ceea925c998bef9aac34
Charlie Astrada <carlosastrada@gmail.com>
2021-05-19 21:26:59 -0300

    Remove hashing from pwd for createUserFixture
    The hash is not needed since the tests we are running call the resolver, which runs the plain-text password through a hash. For higher confidence, remove previous hashing of password from createUserFixture.

--- server/dist/test-utils/fixtures/createUserFixture.js
+++ server/dist/test-utils/fixtures/createUserFixture.js
@@ -1,34 +1,21 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createUserFixture = void 0;
-const argon2_1 = __importDefault(require("argon2"));
 const es_1 = __importDefault(require("faker/locale/es"));
 function createUserFixture() {
-    return __awaiter(this, void 0, void 0, function* () {
-        const firstName = es_1.default.name.firstName().toLowerCase();
-        const lastName = es_1.default.name.lastName().toLowerCase();
-        const fullName = `${firstName}_${lastName}`;
-        const textPwd = es_1.default.internet.password();
-        const password = yield argon2_1.default.hash(textPwd);
-        const email = es_1.default.internet.email().toLowerCase();
-        return {
-            username: fullName,
-            password,
-            email,
-        };
-    });
+    const firstName = es_1.default.name.firstName().toLowerCase();
+    const lastName = es_1.default.name.lastName().toLowerCase();
+    const fullName = `${firstName}_${lastName}`;
+    const password = es_1.default.internet.password();
+    const email = es_1.default.internet.email().toLowerCase();
+    return {
+        username: fullName,
+        password,
+        email,
+    };
 }
 exports.createUserFixture = createUserFixture;
 //# sourceMappingURL=createUserFixture.js.map
--- server/src/test-utils/fixtures/createUserFixture.ts
+++ server/src/test-utils/fixtures/createUserFixture.ts
@@ -1,12 +1,11 @@
-import argon2 from "argon2";
 import faker from "faker/locale/es";
 
-export async function createUserFixture() {
+export function createUserFixture() {
   const firstName = faker.name.firstName().toLowerCase();
   const lastName = faker.name.lastName().toLowerCase();
   const fullName = `${firstName}_${lastName}`;
-  const textPwd = faker.internet.password();
-  const password = await argon2.hash(textPwd);
+  const password = faker.internet.password();
+  // const password = await argon2.hash(textPwd);
   const email = faker.internet.email().toLowerCase();
 
   return {
