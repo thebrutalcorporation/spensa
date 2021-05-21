c5a61f68883ffba36a7602fcb2a9e8e3ccbcd9a7
Charlie Astrada <carlosastrada@gmail.com>
2021-05-21 17:56:27 -0300

    Run prettier formatting on project.
    MIgration files formatted.

--- server/src/migrations/Migration20201126195100.ts
+++ server/src/migrations/Migration20201126195100.ts
@@ -1,10 +1,12 @@
-import { Migration } from '@mikro-orm/migrations';
+import { Migration } from "@mikro-orm/migrations";
 
 export class Migration20201126195100 extends Migration {
-
   async up(): Promise<void> {
-    this.addSql('create table "transaction" ("id" uuid not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "title" text not null);');
-    this.addSql('alter table "transaction" add constraint "transaction_pkey" primary key ("id");');
+    this.addSql(
+      'create table "transaction" ("id" uuid not null, "created_at" timestamptz(0) not null, "updated_at" timestamptz(0) not null, "title" text not null);'
+    );
+    this.addSql(
+      'alter table "transaction" add constraint "transaction_pkey" primary key ("id");'
+    );
   }
-
 }
