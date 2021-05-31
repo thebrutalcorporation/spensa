e649dded1e9ebdf80ca1fda9cc7b90442f55714f
Charlie Astrada <carlosastrada@gmail.com>
2021-05-31 16:24:37 -0300

    refactor: InputField now permits textarea
    The configurable InputField component accepts a prop: textarea (boolean) to indicate if the InputFieldshould include a textArea component. The reason is that there should be both Inputs and Textareas permitted for a long form. The textarea will be used for the create-txn page "details" field for the form.

--- client/src/components/InputField.tsx
+++ client/src/components/InputField.tsx
@@ -2,7 +2,10 @@ import {
   FormControl,
   FormLabel,
   Input,
+  Textarea,
   FormErrorMessage,
+  ComponentWithAs,
+  TextareaProps,
 } from "@chakra-ui/react";
 import { useField } from "formik";
 import React, { InputHTMLAttributes } from "react";
@@ -10,19 +13,25 @@ import React, { InputHTMLAttributes } from "react";
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
   name: string;
+  textarea?: boolean;
 };
 
 const InputField: React.FC<InputFieldProps> = ({
   label,
+  textarea,
   size: _,
   ...props
 }) => {
+  let InputOrTextarea = Input;
+  if (textarea) {
+    (InputOrTextarea as ComponentWithAs<"textarea", TextareaProps>) = Textarea;
+  }
   const [field, { error }] = useField(props);
 
   return (
     <FormControl isInvalid={Boolean(error)}>
       <FormLabel htmlFor={field.name}>{label}</FormLabel>
-      <Input
+      <InputOrTextarea
         {...field}
         {...props}
         id={field.name}
