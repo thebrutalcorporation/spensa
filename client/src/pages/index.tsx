7ea30c30d86a55bfa0d85ae6c72e77cdf2d7f2de
Charlie Astrada <carlosastrada@gmail.com>
2021-05-31 17:35:42 -0300

    feat: add link to create-txn page.

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -4,24 +4,27 @@ import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { useAllTransactionsQuery } from "../generated/graphql";
+import Layout from "../components/Layout";
+import { Link } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 const Index = () => {
   const [{ data }] = useAllTransactionsQuery();
   return (
-    <Box>
-      <NavBar />
-      <Box>
-        <div>Hello World!</div>
-        <br />
-        {!data ? (
-          <div>loading...</div>
-        ) : (
-          data.transactions.map((transaction) => (
-            <div key={transaction.id}>{transaction.title}</div>
-          ))
-        )}
-      </Box>
-    </Box>
+    <Layout>
+      <NextLink href="/create-txn">
+        <Link>Create Transaction</Link>
+      </NextLink>
+      <div>Hello World!</div>
+      <br />
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        data.transactions.map((transaction) => (
+          <div key={transaction.id}>{transaction.title}</div>
+        ))
+      )}
+    </Layout>
   );
 };
 
